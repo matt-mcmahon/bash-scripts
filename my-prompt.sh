6b612429f9cded0342820b2bb4962b68ad2ef2b4
Matt McMahon <contact@matt-mcmahon.com>
2019-11-07 00:10:49 -0500

    Added Hash-Bang to Scripts

--- my-prompt.sh
+++ my-prompt.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source ~/bash-scripts/posh-git.sh
 
 function my-prompt {
--- ssh-permissions.sh
+++ ssh-permissions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/known_hosts
